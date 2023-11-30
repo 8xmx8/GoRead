004f5a15dfc6d9e0fab33e99facc71036490bec5
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-11-30 14:45:43 +0800

    mybatis generation crate mapper interface: -> test success

--- src/main/java/com/yjy/read/mapper/UserMapper.java
+++ src/main/java/com/yjy/read/mapper/UserMapper.java
@@ -3,5 +3,8 @@ package com.yjy.read.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yjy.read.entity.User;
 
+import java.util.List;
+
 public interface UserMapper extends BaseMapper<User> {
+    List<User> selectByName(String name);
 }
