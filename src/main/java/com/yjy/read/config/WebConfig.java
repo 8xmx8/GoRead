62b646d2f37f2382503452f8e3df424afb7d0ad6
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-12-02 17:04:13 +0800

    拦截器

---
+++ src/main/java/com/yjy/read/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.yjy.read.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private MyInterceptor myInterceptor;
+
+    // ctrl+o 查看可重写的方法
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor)
+                .addPathPatterns("/**").excludePathPatterns(
+                        "/login", "/css/*.css", "/js/*.js", "/img/*");
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
